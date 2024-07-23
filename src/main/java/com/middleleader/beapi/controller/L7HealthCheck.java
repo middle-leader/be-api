7a53b6ad4f85634ba816309375017cc40025001f
황정훈 <anthony9307@naver.com>
2024-07-24 00:37:20 +0900

    change default port

--- src/main/java/com/middleleader/beapi/controller/L7HealthCheck.java
+++ src/main/java/com/middleleader/beapi/controller/L7HealthCheck.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class L7HeathCheck {
+public class L7HealthCheck {
 
 	@GetMapping("/l7-healthcheck")
 	public HttpStatus getHealthCheck() {
