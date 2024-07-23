5364eea3f4db6df2789d3ea696126c0c78e6dc3c
황정훈 <anthony9307@naver.com>
2024-07-23 23:34:08 +0900

    add healthCheck

---
+++ src/main/java/com/middleleader/beapi/controller/L7HeathCheck.java
@@ -0,0 +1,14 @@
+package com.middleleader.beapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class L7HeathCheck {
+
+	@GetMapping("/l7-healthcheck")
+	public HttpStatus getHealthCheck() {
+		return HttpStatus.OK;
+	}
+}
